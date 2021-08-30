94e559835f1bab79cfa5f6b2876d36c6629238a8
Tatiaynti <tatianamo1234@mail.ru>
2021-08-30 14:58:58 +0300

    рефакторинг фильтров на карточке

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -3,16 +3,6 @@ import {changeDateFormatToYear, sliceDescription} from '../utils/utils-for-rende
 
 const filmCardTemplate = (film) => {
   const {title, rating, release, runtime, genres, description, poster, comments, isFavorite, isInWatchlist, isWatched} = film;
-  const favoriteClassName = isFavorite
-    ? 'film-card__controls-item--active'
-    : '';
-  const watchlistClassName = isInWatchlist
-    ? 'film-card__controls-item--active'
-    : '';
-  const watchedClassName = isWatched
-    ? 'film-card__controls-item--active'
-    : '';
-
   return `
   <article class="film-card">
     <h3 class="film-card__title">${title}</h3>
@@ -26,9 +16,9 @@ const filmCardTemplate = (film) => {
     <p class="film-card__description">${sliceDescription(description)}</p>
     <a class="film-card__comments">${comments.length} comments</a>
     <div class="film-card__controls">
-      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${watchlistClassName}" type="button">Add to watchlist</button>
-      <button class="film-card__controls-item film-card__controls-item--mark-as-watched ${watchedClassName}" type="button">Mark as watched</button>
-      <button class="film-card__controls-item film-card__controls-item--favorite ${favoriteClassName}" type="button">Mark as favorite</button>
+      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${isInWatchlist ? 'film-card__controls-item--active' : ''}" type="button">Add to watchlist</button>
+      <button class="film-card__controls-item film-card__controls-item--mark-as-watched ${isWatched ? 'film-card__controls-item--active' : ''}" type="button">Mark as watched</button>
+      <button class="film-card__controls-item film-card__controls-item--favorite ${isFavorite ? 'film-card__controls-item--active' : ''}" type="button">Mark as favorite</button>
     </div>
   </article>
 `;
