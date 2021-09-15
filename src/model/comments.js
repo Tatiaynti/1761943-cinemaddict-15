46bba9be2e8b2cb6aaef828afbd45796e8262f9b
Tatiaynti <tatianamo1234@mail.ru>
2021-09-15 16:29:14 +0300

    создана модель комментариев

---
+++ src/model/comments.js
@@ -0,0 +1,38 @@
+import AbstractObserver from '../utils/abstract-observer.js';
+
+export default class Comments extends AbstractObserver {
+  constructor() {
+    super();
+    this._commentsList = [];
+  }
+
+  setComments(commentsList) {
+    if (commentsList === null) {
+      this._commentsList = commentsList;
+    }
+    this._commentsList = commentsList.slice();
+  }
+
+  getComments() {
+    return this._commentsList;
+  }
+
+  addComment(update) {
+    this._commentsList = [
+      ...this._commentsList,
+      update,
+    ];
+  }
+
+  deleteComments(updateId) {
+    const index = this._commentsList.findIndex((comments) => comments.id === updateId);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting comment');
+    }
+    this._commentsList = [
+      ...this._commentsList.slice(0, index),
+      ...this._commentsList.slice(index + 1),
+    ];
+  }
+}
