fdd85a84c632ac05f113033de360559b3cc6511e
Tatiaynti <tatianamo1234@mail.ru>
2021-09-17 11:01:49 +0300

    view refactoring

--- src/view/film-list-extra.js
+++ src/view/film-list-extra.js
@@ -3,8 +3,6 @@ import AbstractView from './abstract.js';
 const filmListExtraTemplate = (title) => (
   `<section class="films-list films-list--extra">
     <h2 class="films-list__title">${title}</h2>
-    <div class="films-list__container">
-    </div>
   </section>`
 );
 
